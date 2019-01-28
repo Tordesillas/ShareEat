eb7ba59fd30d07b3a7cc70fe391ad65d2f74fe36
doryanB <bonifassi.doryan@gmail.com>
2019-01-28 11:00:17 +0100

    change eventList

--- components/EventList.js
+++ components/EventList.js
@@ -1,115 +1,132 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 import Colors from "../constants/Colors";
 import moment from "moment";
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCalendarAlt, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
-import {getImageFromName} from "../constants/Images";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faCalendarAlt,
+  faMapMarkerAlt
+} from "@fortawesome/free-solid-svg-icons";
+import { getImageFromName } from "../constants/Images";
 import Users from "../constants/Users";
-require('moment/locale/fr');
-
+require("moment/locale/fr");
 
 export default class EventList extends React.Component {
-    render() {
-        moment.locale("fr");
-        const events = this.retrieveEvents();
+  render() {
+    moment.locale("fr");
+    const events = this.retrieveEvents();
 
-        return (
-            <View style={styles.main_container}>
-                <View style={styles.main_container}>
-                    {
-                        events.map((item) => {
-                            const eventDate = new Date(item.date);
-                            const formattedEventDate = moment()
-                                .year(eventDate.getFullYear())
-                                .month(eventDate.getMonth())
-                                .date(eventDate.getDate())
-                                .format("dddd D MMMM YYYY");
+    return (
+      <View style={styles.main_container}>
+        <View style={styles.main_container}>
+          {events.map(item => {
+            const eventDate = new Date(item.date);
+            const formattedEventDate = moment()
+              .year(eventDate.getFullYear())
+              .month(eventDate.getMonth())
+              .date(eventDate.getDate())
+              .format("dddd D MMMM YYYY");
 
-                            return (
-                                <TouchableOpacity style={styles.flatListRow} key={item.id} onPress={() => {
-                                    this.props.navigation.navigate("Event", { event: item })
-                                }}>
-                                    <View style={styles.eventDetails}>
-                                        <View>
-                                            <Image style={styles.eventImage} source={getImageFromName(Users[item.organizer].photoId)} />
-                                        </View>
-                                        <View>
-                                            <Text style={styles.flatListLabel}>{item.name}</Text>
+            return (
+              <TouchableOpacity
+                style={styles.flatListRow}
+                key={item.id}
+                onPress={() => {
+                  item.type === "dpp"
+                    ? this.props.navigation.navigate("Event", { event: item })
+                    : null;
+                }}
+              >
+                <View style={styles.eventDetails}>
+                  <View>
+                    <Image
+                      style={styles.eventImage}
+                      source={getImageFromName(Users[item.organizer].photoId)}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles.flatListLabel}>{item.name}</Text>
 
-                                            <View style={styles.eventDetail}>
-                                                <FontAwesomeIcon icon={faCalendarAlt} style={styles.icon} />
-                                                <Text style={styles.flatListItem}>{formattedEventDate}</Text>
-                                            </View>
+                    <View style={styles.eventDetail}>
+                      <FontAwesomeIcon
+                        icon={faCalendarAlt}
+                        style={styles.icon}
+                      />
+                      <Text style={styles.flatListItem}>
+                        {formattedEventDate}
+                      </Text>
+                    </View>
 
-                                            <View style={styles.eventDetail}>
-                                                <FontAwesomeIcon icon={faMapMarkerAlt} style={styles.icon} />
-                                                <Text style={styles.flatListItem}>{item.location}</Text>
-                                            </View>
-                                        </View>
-                                    </View>
-                                </TouchableOpacity>
-                            );
-                        })
-                    }
+                    <View style={styles.eventDetail}>
+                      <FontAwesomeIcon
+                        icon={faMapMarkerAlt}
+                        style={styles.icon}
+                      />
+                      <Text style={styles.flatListItem}>{item.location}</Text>
+                    </View>
+                  </View>
                 </View>
-            </View>
-        );
-    }
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
 
-    retrieveEvents() {
-        if (this.props.screen === "home") {
-            return [this.props.events[0], this.props.events[1]];
-        } else {
-            return this.props.events;
-        }
+  retrieveEvents() {
+    if (this.props.screen === "home") {
+      return [this.props.events[0], this.props.events[1]];
+    } else {
+      return this.props.events;
     }
+  }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        flex: 1,
-        marginBottom: 10
-    },
-    flatList: {},
-    flatListRow: {
-        backgroundColor: Colors.DARK_MEDIUM_BLUE,
-        padding: 10,
-        marginTop: 10,
-        marginBottom: 10,
-        borderRadius: 5,
-    },
-    flatListLabel: {
-        color: Colors.CORAL,
-        fontWeight: "bold",
-        fontSize: 18,
-        marginBottom: 5,
-    },
-    flatListItem: {
-        color: Colors.WHITE,
-        fontSize: 15
-    },
-    eventDetails: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignContent: 'center',
-    },
-    eventDetail: {
-        flexDirection: 'row',
-        marginBottom: 5,
-    },
-    eventImage: {
-        marginRight: 15,
-        borderRadius: 100,
-        width: 50,
-        height: 50
-    },
-    eventDescription: {
-        color: Colors.WHITE
-    },
-    icon: {
-        marginRight: 5,
-        color: Colors.WHITE,
-        width: 10
-    }
+  main_container: {
+    flex: 1,
+    marginBottom: 10
+  },
+  flatList: {},
+  flatListRow: {
+    backgroundColor: Colors.DARK_MEDIUM_BLUE,
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 5
+  },
+  flatListLabel: {
+    color: Colors.CORAL,
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 5
+  },
+  flatListItem: {
+    color: Colors.WHITE,
+    fontSize: 12
+  },
+  eventDetails: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "center"
+  },
+  eventDetail: {
+    flexDirection: "row",
+    marginBottom: 5
+  },
+  eventImage: {
+    marginRight: 15,
+    borderRadius: 100,
+    width: 50,
+    height: 50
+  },
+  eventDescription: {
+    color: Colors.WHITE
+  },
+  icon: {
+    marginRight: 5,
+    color: Colors.WHITE,
+    width: 10
+  }
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,87 +1,106 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
 import Colors from "../constants/Colors";
 import EventList from "../components/EventList";
 import MainTitle from "../components/MainTitle";
 import Events from "../constants/Events";
-import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons/index";
 import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
 import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome/index";
 
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {
-        title: 'ShareEat',
-        headerStyle: { backgroundColor: Colors.DARK_MEDIUM_BLUE },
-        headerTitleStyle: { color: Colors.WHITE }
-    };
+  static navigationOptions = {
+    title: "ShareEat",
+    headerStyle: { backgroundColor: Colors.DARK_MEDIUM_BLUE },
+    headerTitleStyle: { color: Colors.WHITE }
+  };
 
-    render() {
-        return (
-            <View style={styles.main_container}>
-                <ScrollView style={styles.container}>
-                    <MainTitle title={"Créer ou rejoindre un repas"} />
-                    <View style={styles.dinnerContainer}>
-                        <TouchableOpacity style={styles.button} onPress={() => {this.props.navigation.navigate("NotImplemented")}}>
-                            <FontAwesomeIcon icon={faPlus} style={styles.icon} size={30} />
-                            <Text style={styles.buttonText}>Créer</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.button} onPress={() => {this.props.navigation.navigate("SearchEvent")}}>
-                            <FontAwesomeIcon icon={faSearch} style={styles.icon} size={30} />
-                            <Text style={styles.buttonText}>Rejoindre</Text>
-                        </TouchableOpacity>
-                    </View>
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <ScrollView style={styles.container}>
+          <MainTitle title={"Créer ou rejoindre un repas"} />
+          <View style={styles.dinnerContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                this.props.navigation.navigate("NotImplemented");
+              }}
+            >
+              <FontAwesomeIcon icon={faPlus} style={styles.icon} size={30} />
+              <Text style={styles.buttonText}>Créer</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                this.props.navigation.navigate("SearchEvent");
+              }}
+            >
+              <FontAwesomeIcon icon={faSearch} style={styles.icon} size={30} />
+              <Text style={styles.buttonText}>Rejoindre</Text>
+            </TouchableOpacity>
+          </View>
 
-                    <View style={styles.events}>
-                        <MainTitle title={"Événements à venir"} />
-                        <EventList events={Events} screen={"home"} navigation={this.props.navigation}/>
-                    </View>
-                </ScrollView>
-            </View>
-        );
-    }
+          <View style={styles.events}>
+            <MainTitle title={"Événements à venir"} />
+            <EventList
+              events={Events}
+              screen={"home"}
+              navigation={this.props.navigation}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        flex: 1,
-        backgroundColor: Colors.DARK_BLUE,
-        justifyContent: 'center',
-        alignItems: "stretch",
-        flexDirection: 'column',
-    },
-    container: {
-        padding: 15
-    },
-    dinnerContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-        marginBottom: 15
-    },
-    button: {
-        backgroundColor: Colors.DARK_MEDIUM_BLUE,
-        marginTop: 15,
-        marginLeft: 30,
-        marginRight: 30,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingTop: 20,
-        paddingBottom: 20,
-        borderRadius: 5,
-        fontSize: 16,
-        width: 120
-    },
-    buttonText: {
-        color: Colors.WHITE,
-        textTransform: "uppercase",
-        fontWeight: "bold",
-        fontSize: 16,
-    },
-    icon: {
-        color: Colors.CORAL,
-        marginBottom: 15
-    },
-    events: {
-        flex: 1
-    }
+  main_container: {
+    flex: 1,
+    backgroundColor: Colors.DARK_BLUE,
+    justifyContent: "center",
+    alignItems: "stretch",
+    flexDirection: "column"
+  },
+  container: {
+    padding: 15
+  },
+  dinnerContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 15
+  },
+  button: {
+    backgroundColor: Colors.DARK_MEDIUM_BLUE,
+    marginTop: 15,
+    marginLeft: 30,
+    marginRight: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 20,
+    paddingBottom: 20,
+    borderRadius: 5,
+    fontSize: 16,
+    width: 120
+  },
+  buttonText: {
+    color: Colors.WHITE,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  icon: {
+    color: Colors.CORAL,
+    marginBottom: 15
+  },
+  events: {
+    flex: 1
+  }
 });
